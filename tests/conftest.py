b9c38f6b5af84971115918250c26d82ca27b94ed
Zoe Yanai <zoe1999@gmail.com>
2026-07-13 20:14:46 +0100

    create mock data for unit testing

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import json
+import pytest
+import os
+
+@pytest.fixture
+def load_mock_data():
+    def _loader(filename):
+        path = os.path.join(os.path.dirname(__file__), 'mock_data', filename)
+        with open(path, 'r') as file:
+            return json.load(file)
+        return _loader
